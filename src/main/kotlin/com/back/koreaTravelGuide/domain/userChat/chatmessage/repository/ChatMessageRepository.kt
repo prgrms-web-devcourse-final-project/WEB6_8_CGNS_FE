3b981accc29553b69bc4a1db6be6d4839d6cd933
beekeeper24 <ahwnsk94@gmail.com>
2025-09-26 16:23:22 +0900

    feat(be): @Repository 어노테이션 추가

--- src/main/kotlin/com/back/koreaTravelGuide/domain/userChat/chatmessage/repository/ChatMessageRepository.kt
+++ src/main/kotlin/com/back/koreaTravelGuide/domain/userChat/chatmessage/repository/ChatMessageRepository.kt
@@ -2,7 +2,9 @@ package com.back.koreaTravelGuide.domain.userChat.chatmessage.repository
 
 import com.back.koreaTravelGuide.domain.userChat.chatmessage.entity.ChatMessage
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ChatMessageRepository : JpaRepository<ChatMessage, Long> {
     fun findTop50ByRoomIdOrderByIdDesc(roomId: Long): List<ChatMessage>
 
