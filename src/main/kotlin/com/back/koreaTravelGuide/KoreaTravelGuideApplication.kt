a59dcd2d1fdf7af0d36b2662b7166eeaf3defdf7
JIWONKIMS <giwon11292@gmail.com>
2025-09-26 11:48:39 +0900

    bug(be): application 위치 조정

--- src/main/kotlin/com/back/koreaTravelGuide/KoreaTravelGuideApplication.kt
+++ src/main/kotlin/com/back/koreaTravelGuide/KoreaTravelGuideApplication.kt
@@ -1,11 +1,15 @@
-package com.back.koreaTravelGuide.application
+package com.back.koreaTravelGuide
 
 // TODO: 메인 애플리케이션 클래스 - 스프링 부트 시작점 및 환경변수 로딩
 import io.github.cdimascio.dotenv.dotenv
+import org.springframework.ai.model.chat.memory.repository.jdbc.autoconfigure.JdbcChatMemoryRepositoryAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["com.back.koreaTravelGuide"])
+@SpringBootApplication(
+    scanBasePackages = ["com.back.koreaTravelGuide"],
+    exclude = [JdbcChatMemoryRepositoryAutoConfiguration::class],
+)
 class KoreaTravelGuideApplication
 
 fun main(args: Array<String>) {
